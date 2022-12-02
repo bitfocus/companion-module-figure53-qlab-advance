e6cd32aaef49b666985373a78306cd78ad64961c
John A Knight Jr <istnv@ayesti.com>
2022-12-02 02:10:03 -0800

    Chore: Prettier

--- choices.js
+++ choices.js
@@ -1,32 +1,31 @@
 module.exports = {
-
 	CONTINUE_MODE: [
 		{ label: 'Do Not Continue', id: '0' },
-		{ label: 'Auto Continue',   id: '1' },
-		{ label: 'Auto Follow',     id: '2' }
+		{ label: 'Auto Continue', id: '1' },
+		{ label: 'Auto Follow', id: '2' },
 	],
 
 	TOGGLE: [
-		{ id: 1, label: 'On'},
-		{ id: 0, label: 'Off'},
-		{ id: 2, label: 'Toggle'}
+		{ id: 1, label: 'On' },
+		{ id: 0, label: 'Off' },
+		{ id: 2, label: 'Toggle' },
 	],
 
 	ON_OFF: [
-		{ id: 1, label: 'On'},
-		{ id: 0, label: 'Off'}
+		{ id: 1, label: 'On' },
+		{ id: 0, label: 'Off' },
 	],
 
 	OVERRIDE: [
-		{ id: 'midiInputEnabled', 	label: 'Midi Input' },
-		{ id: 'midiOutputEnabled',	label: 'Midi Output' },
-		{ id: 'mscInputEnabled',	label: 'MSC Input' },
-		{ id: 'mscOutputEnabled', 	label: 'MSC Output' },
-		{ id: 'sysexInputEnabled',	label: 'SysEx Input' },
-		{ id: 'sysexOutputEnabled',	label: 'SysEx Output' },
-		{ id: 'oscOutputEnabled',	label: 'OSC Output' },
+		{ id: 'midiInputEnabled', label: 'Midi Input' },
+		{ id: 'midiOutputEnabled', label: 'Midi Output' },
+		{ id: 'mscInputEnabled', label: 'MSC Input' },
+		{ id: 'mscOutputEnabled', label: 'MSC Output' },
+		{ id: 'sysexInputEnabled', label: 'SysEx Input' },
+		{ id: 'sysexOutputEnabled', label: 'SysEx Output' },
+		{ id: 'oscOutputEnabled', label: 'OSC Output' },
 		{ id: 'timecodeInputEnabled', label: 'Timecode Input' },
 		{ id: 'timecodeOutputEnabled', label: 'Timecode Output' },
-		{ id: 'artNetEnabled', 		label: 'Art-Net Enabled' }
-	]
-};
+		{ id: 'artNetEnabled', label: 'Art-Net Enabled' },
+	],
+}
--- variables.js
+++ variables.js
@@ -1,97 +1,95 @@
 module.exports = {
-
-	setVariables: function() {
-
+	setVariables: function () {
 		var variables = [
 			{
 				label: 'Version of QLab attached to this instance',
-				name:  'q_ver'
+				name: 'q_ver',
 			},
 			{
 				label: 'Double Go Minimum Time',
-				name:  'min_go'
+				name: 'min_go',
 			},
 			{
 				label: 'Playhead Cue UniqueID',
-				name:  'n_id'
+				name: 'n_id',
 			},
 			{
 				label: 'Playhead Cue Name',
-				name:  'n_name'
+				name: 'n_name',
 			},
 			{
 				label: 'Playhead Cue Number',
-				name:  'n_num'
+				name: 'n_num',
 			},
 			{
 				label: 'Playhead Cue Type',
-				name:  'n_type'
+				name: 'n_type',
 			},
 			{
 				label: 'Playhead Notes',
-				name:  'n_notes'
+				name: 'n_notes',
 			},
 			{
 				label: 'Playhead Cue Status',
-				name:  'n_stat'
+				name: 'n_stat',
 			},
 			{
 				label: 'Running Cue UniqueID',
-				name:  'r_id'
+				name: 'r_id',
 			},
 			{
 				label: 'Running Cue Name',
-				name:  'r_name'
+				name: 'r_name',
 			},
 			{
 				label: 'Running Cue Number',
-				name:  'r_num'
+				name: 'r_num',
 			},
 			{
 				label: 'Running Cue Status',
-				name:  'r_stat'
+				name: 'r_stat',
 			},
 			{
 				label: 'Running Cue Time left, variable size',
-				name:  'r_left'
+				name: 'r_left',
 			},
 			{
 				label: 'Running Cue Time left, HH:MM:SS',
-				name:  'r_hhmmss'
+				name: 'r_hhmmss',
 			},
 			{
 				label: 'Running Cue Time left, Hour',
-				name:  'r_hh'
+				name: 'r_hh',
 			},
 			{
 				label: 'Running Cue Time left, Minute',
-				name:  'r_mm'
+				name: 'r_mm',
 			},
 			{
 				label: 'Running Cue Time left, Second',
-				name:  'r_ss'
+				name: 'r_ss',
 			},
 			{
 				label: 'Running Cue Elapsed Time, variable size',
-				name:  'e_time'
+				name: 'e_time',
 			},
 			{
 				label: 'Running Cue Elapsed Time, HH:MM:SS',
-				name:  'e_hhmmss'
+				name: 'e_hhmmss',
 			},
 			{
 				label: 'Running Cue Elapsed Time, Hour',
-				name:  'e_hh'
+				name: 'e_hh',
 			},
 			{
 				label: 'Running Cue Elapsed Time, Minute',
-				name:  'e_mm'
+				name: 'e_mm',
 			},
 			{
 				label: 'Running Cue Elapsed Time, Second',
-				name:  'e_ss'
-			}
-		];
-		return(variables);
-	}
-};
+				name: 'e_ss',
+			},
+		]
+		return variables
+	},
+}
